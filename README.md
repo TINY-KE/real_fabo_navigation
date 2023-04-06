57482b4663f962ca7d8b75d966ee2fb68e1f1aca
TINY-KE <zhangjiadong@buaa.edu.cn>
2023-04-06 21:02:52 +0800

    现存问题

--- README.md
+++ README.md
@@ -7,9 +7,15 @@
 + 启动键盘控制器，话题为/turtle1/cmd_vel
     `rosrun turtlesim turtle_teleop_key`
 
-## 二、注意：
-对于真实场景的机器人的cartographer，use_sim_time设置为false；gazebo中设置为true。
-
+## 二、注意的问题：
++ 对于真实场景的机器人的cartographer，use_sim_time设置为false；gazebo中设置为true。
++ 在BtManager.cpp的compute()函数中，可以修改“轮间距”和“速度的修正系数”
+    按理说，fabo机器人出厂时已经标定过里程计了，直接发送“速度”就可以得到比较准确的移动效果。但是课题三的小胖机器人装了手臂，所以我们又标定了下，给左右轮速度加了个“修正系数”。
++ 在fabo_robot/config/fabo/base_local_planner_params3.yaml中，修改局部路径规划发布/cmd_vel的频率，目前为5
+  + 发布/cmd_vel的频率，不能过高。超过10，很容易导致安卓平板死机。
+  + 不确定是蓝牙的问题，还是安卓平板的性能问题。可以尝试通过更换为wifi通信的方式。
++ 
+  
 ## 三、建图：
 + 启动雷达【内部包含了雷达和机器人的tf变换】
 
--- fabo_robot/src/BtManager.cpp
+++ fabo_robot/src/BtManager.cpp
@@ -8,12 +8,12 @@ BtManager::BtManager(int &blueteeth_): blueteeth(blueteeth_)
 }
 
 void BtManager::set_ros_node(ros::NodeHandle& n){
-    // on cmd_vel
     sub_cmdvel = n.subscribe("/cmd_vel", 1000, &BtManager::callback, this);
     sub_cmdVel_keybord = n.subscribe("/turtle1/cmd_vel", 1, &BtManager::cmdVel_callback, this);
     sub_head = n.subscribe("/mam_angle", 1, &BtManager::head_callback, this);
 }
 
+// 接收键盘的速度指令，控制底盘的移动
 void BtManager::cmdVel_callback(const geometry_msgs::Twist& msg) {
     // normal : vl = 2/-2, z = 2/-2, D = 500, kl = 0.01, ka = 1.5
     // vl = kl * (v_left + v_right) / 2, va = ka * (v_right - v_left) / D
@@ -33,6 +33,7 @@ void BtManager::cmdVel_callback(const geometry_msgs::Twist& msg) {
     sendBtData(order);
 }
 
+// 控制头的转动角度
 void BtManager::head_callback(const std_msgs::Float64& msg) {
     int angle;
     angle =  msg.data/*逆时针-90~90*/  *(-1) +90/*顺时针0~180*/ + 30;
@@ -45,6 +46,40 @@ void BtManager::head_callback(const std_msgs::Float64& msg) {
     sendBtData(order);
 }
 
+
+// 接收movebase的速度指令，控制底盘的移动
+void BtManager::callback(const geometry_msgs::Twist &msg){
+    std::string wheel_speed_r = "";
+    std::string wheel_speed_l = "";
+    // bluetooth_message = bluetooth_message                     + msg.name + ","                 + to_string(msg.current_phase) + "/" + to_string(msg.total_phase);
+    compute( msg.linear.x,  msg.angular.z  ,  wheel_speed_r,  wheel_speed_l);
+    int time = 200; //cmd_vel的持续时长.单位ms
+    double  timeStamp = ros::Time::now().toSec();
+    string move_order = "Wheel," + wheel_speed_l +   "," + wheel_speed_r + "," + std::to_string(time)+
+                                "," + std::to_string(timeStamp);
+    cout << "cmdvel.x=" << msg.linear.x << " cmdvel.ywa=" << msg.angular.z <<" ,order = " << move_order << endl;
+    // printInColor("蓝牙发送:", RED, wheel_speed_l , wheel_speed_r);
+    sendBtData(move_order);
+    return;
+}
+void BtManager::compute(const double linear_x, const  double angular_z ,string& wheel_speed_r, string& wheel_speed_l ){
+
+    double width_robot = 0.447 ; //447mm 。  速度的单位是m/s 
+
+    double vel_x = linear_x;
+    double vel_th = angular_z;
+    double right_vel = 0.0;
+    double left_vel = 0.0;
+    left_vel = vel_x - vel_th * width_robot / 2.0;  left_vel = left_vel *1000.0 *1.0585;  /* TODO: 其实标定出的结果是1.0385 */
+    right_vel = vel_x + vel_th * width_robot / 2.0; right_vel = right_vel*1000.0 *1.0759;
+ 
+    
+    wheel_speed_l = to_string(left_vel);
+    wheel_speed_r = to_string(right_vel);
+}
+
+
+// 通过蓝牙通道发送指令
 void BtManager::sendBtData(string str){
     int length = str.size();
     char *temp = (char*)str.c_str();
@@ -61,38 +96,5 @@ void BtManager::sendBtData(string str){
     printInColor("发送完成\n\n", BLUE);
 }
 
-// private
-void BtManager::compute(const double linear_x, const  double angular_z ,string& wheel_speed_r, string& wheel_speed_l ){
-
-    double width_robot = 0.447 ; //447mm  TODO:  速度的单位是m/s ???
-
-    //copy from https://blog.csdn.net/qq_34935373/article/details/107605615
-    // 订阅cmd_val下的geometry_msgs::Twist消息 ，并且实际转化为左右轮的速度，以下是转换的源码。
-    double vel_x = linear_x;
-    double vel_th = angular_z;
-    double right_vel = 0.0;
-    double left_vel = 0.0;
-    left_vel = vel_x - vel_th * width_robot / 2.0;  left_vel = left_vel *1000.0 *1.0585;  /* TODO: 其实标定出的结果是1.0385 */
-    right_vel = vel_x + vel_th * width_robot / 2.0; right_vel = right_vel*1000.0 *1.0759;
- 
-    
-    wheel_speed_l = to_string(left_vel);
-    wheel_speed_r = to_string(right_vel);
-}
-
-void BtManager::callback(const geometry_msgs::Twist &msg){
-    std::string wheel_speed_r = "";
-    std::string wheel_speed_l = "";
-    // bluetooth_message = bluetooth_message                     + msg.name + ","                 + to_string(msg.current_phase) + "/" + to_string(msg.total_phase);
-    compute( msg.linear.x,  msg.angular.z  ,  wheel_speed_r,  wheel_speed_l);
-    int time = 200; //cmd_vel的持续时长.单位ms
-    double  timeStamp = ros::Time::now().toSec();
-    string move_order = "Wheel," + wheel_speed_l +   "," + wheel_speed_r + "," + std::to_string(time)+
-                                "," + std::to_string(timeStamp);
-    cout << "cmdvel.x=" << msg.linear.x << " cmdvel.ywa=" << msg.angular.z <<" ,order = " << move_order << endl;
-    // printInColor("蓝牙发送:", RED, wheel_speed_l , wheel_speed_r);
-    sendBtData(move_order);
-    return;
-}
 
 };
